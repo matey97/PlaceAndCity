2e31c9d46eaf8bd673232e52b455621ba798beb2
Miguel Matey Sanz <al341802@uji.es>
2022-03-09 10:40:56 +0100

    Add flex wrap to area status list enable download all button on all ready

--- src/app/view/information-panel/areas-list-finish/areas-list-finish.component.ts
+++ src/app/view/information-panel/areas-list-finish/areas-list-finish.component.ts
@@ -12,5 +12,10 @@ export class AreasListFinishComponent {
   @Input() drawnAreas: InterestArea[] = [];
   @Input() commandStatus: Map<string, CommandStatus> = new Map<string, CommandStatus>();
 
+  get allDownloadsReady(): boolean {
+    const commands = Array.from(this.commandStatus.values());
+    return commands.indexOf(CommandStatus.SUBMITTED) === -1;
+  }
+
   constructor() { }
 }
