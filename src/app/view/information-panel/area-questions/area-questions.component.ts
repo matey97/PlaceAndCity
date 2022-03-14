84f0156a5a5c84832b161d02d9277deb630fa03a
Miguel Matey Sanz <al341802@uji.es>
2022-03-14 12:44:01 +0100

    Include text questions in area questions component

--- src/app/view/information-panel/area-questions/area-questions.component.ts
+++ src/app/view/information-panel/area-questions/area-questions.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { likertQuestions } from "./likert-questions";
 import { AreaAnswers } from "../../../model/area";
+import { nameQuestion, freeQuestions } from "./text-questions";
+import { Question } from "./question";
 
 @Component({
   selector: 'app-area-questions',
@@ -13,48 +15,52 @@ export class AreaQuestionsComponent implements OnInit {
   @Output() answers = new EventEmitter<AreaAnswers>()
 
   nameForm!: FormGroup;
-  freeText!: FormGroup;
-  questions!: FormGroup;
+  freeQuestionsForm!: FormGroup;
+  likertQuestionsForm!: FormGroup;
 
+  nameQuestion = nameQuestion;
+  freeQuestions = freeQuestions;
   likertQuestions = likertQuestions;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.nameForm = this.formBuilder.group({
-      name: ["", Validators.required]
-    });
+    this.nameForm = this.formBuilder.group(this.formConfigBuilder([this.nameQuestion]));
 
-    this.freeText = this.formBuilder.group({
-      text: ["", Validators.required]
-    });
+    this.freeQuestionsForm = this.formBuilder.group(this.formConfigBuilder(this.freeQuestions));
 
-    const questionsConfig: {[p: string]: any} = {};
-    this.likertQuestions.forEach((question) => {
-      const config: any = [""]
-      if (question.required) config.push(Validators.required);
-
-      questionsConfig[question.id] = config;
-    });
-    this.questions = this.formBuilder.group(questionsConfig);
+    this.likertQuestionsForm = this.formBuilder.group(this.formConfigBuilder(this.likertQuestions));
   }
 
   onQuestionsAnswered() {
     this.answers.emit({
       name: this.nameForm.value["name"],
-      freeText: this.freeText.value["text"],
-      questions: this.questions.value
+      freeText: this.freeQuestionsForm.value["text"],
+      questions: this.likertQuestionsForm.value
     });
   }
 
   onNextInQuestions() {
     this.likertQuestions.forEach((likertQuestion) => {
-      this.questions.controls[likertQuestion.id].markAsTouched();
+      this.likertQuestionsForm.controls[likertQuestion.id].markAsTouched();
     });
   }
 
   inValidState(questionId: string): boolean {
-    const control = this.questions.controls[questionId];
+    const control = this.likertQuestionsForm.controls[questionId];
     return control.valid || control.untouched;
   }
+
+  private formConfigBuilder(questions: Question[]): {[p: string]: any} {
+    const questionsConfig: {[p: string]: any} = {};
+    questions.forEach((question) => {
+      const config: any = [""]
+      if (question.required) config.push(Validators.required);
+
+      questionsConfig[question.id] = config;
+    });
+
+    return questionsConfig;
+  }
+
 }
